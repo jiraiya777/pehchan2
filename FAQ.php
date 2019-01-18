124e15e304bd695439d4014f18e93ca3621dd646
jiraiya777 <2017csb1103@iitrpr.ac.in>
2019-01-18 18:26:33 +0530

    j

--- FAQ.php
+++ FAQ.php
@@ -5,7 +5,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="bootstrap.css">
-    <link rel="stylesheet"  href="FAQstyle.css">
+    <link rel="stylesheet"  href="../FAQstyle.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
@@ -65,13 +65,13 @@ include 'Siteheader.php';
             <br>
 
             <div class="cam7" style="cursor: pointer;">
-             <mfs>Who is the Pehchaan-Ek Safar team?</mfs>
+             <mfs>How is Pehchaan-Ek Safar funded?</mfs>
             <img onclick="myFunction()" src="7.jpg" alt="Smiley face" height="25" width="30" style="cursor: pointer;">
             </div>
             <div class="cam8" style="cursor: pointer;">
-                <mfs>Who is the Pehchaan-Ek Safar team?</mfs>       
+                <mfs>How is Pehchaan-Ek Safar funded?</mfs>       
               <img onclick="myFunction()" src="6.jpg" alt="Smiley face" height="30" width="30" style="cursor: pointer;">
-              <br>Our team presently involves not only students but also couple of professors.The group is fomally underpinned by TBIF-IIT Ropar.
+              <br>At present, we are supported by voluntary donations made by IIT Ropar’s fraternity.
             </div>
             <br>
 
